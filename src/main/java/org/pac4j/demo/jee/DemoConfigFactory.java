e031e1c0daf195c57d50c9294bd0a927ee6417a5
Jérôme LELEU <leleuj@gmail.com>
2020-11-17 17:34:04 +0100

    update multi profile config

--- src/main/java/org/pac4j/demo/jee/DemoConfigFactory.java
+++ src/main/java/org/pac4j/demo/jee/DemoConfigFactory.java
@@ -57,6 +57,7 @@ public class DemoConfigFactory implements ConfigFactory {
         final SAML2Client saml2Client = new SAML2Client(cfg);
 
         final FacebookClient facebookClient = new FacebookClient("145278422258960", "be21409ba8f39b5dae2a7de525484da8");
+        facebookClient.setMultiProfile(true);
         final TwitterClient twitterClient = new TwitterClient("CoxUiYwQOSFDReZYdjigBA", "2kAzunH5Btc4gRSaMr7D7MkyoJ5u1VzbOOzE8rBofs");
         // HTTP
         final FormClient formClient = new FormClient("http://localhost:8080/loginForm.jsp", new SimpleTestUsernamePasswordAuthenticator());
